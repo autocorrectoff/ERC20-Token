d75fd6762372e56a465837d77112163c54031ed0
LAPTOP-QLEGTRSJ\radio <branko.miric@creen.io>
2023-07-31 10:52:40 +0200

    added dotenv; updated hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+
+const privateKey = '0x70a7e3ac2da728f5001df87c0cec7b07ebe44dcc3de2e08c1cf5d38a053e4f8c';
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: '0.8.9',
+  paths: {
+    artifacts: './artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    rinkeby: {
+      url: 'https://speedy-nodes-nyc.moralis.io/68be5cfb5f3e5bf20da7d6b0/eth/rinkeby',
+      accounts: [privateKey],
+    },
+  //   mumbai: {},
+  //   polygon: {}
+  }
 };
